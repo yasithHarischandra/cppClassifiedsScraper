05e63ca4304c44c13bda29d3ecbcff295c3a1b09
Yasith Harischandra <yasith.harischandra@outlook.com>
2024-12-22 22:01:37 +0530

    Fixed error where some classified pages are not parsed correctly.

--- cppWebScraper/Scraper_WebPageLoader.cpp
+++ cppWebScraper/Scraper_WebPageLoader.cpp
@@ -10,7 +10,7 @@ Scraper_WebPageLoader::Scraper_WebPageLoader(const std::string& aUrl)
 	// make the HTTP request to retrieve the target page
 	cpr::Response response = cpr::Get(cpr::Url{ aUrl }, myPageHeader);
 
-	myHtmlDoc = htmlReadMemory(response.text.c_str(), (int)response.text.length(), nullptr, "UNICODE", HTML_PARSE_NOWARNING | HTML_PARSE_NOERROR);
+	myHtmlDoc = htmlReadMemory(response.text.c_str(), (int)response.text.length(), nullptr, "UNICODE", HTML_PARSE_NOWARNING | HTML_PARSE_NOERROR | HTML_PARSE_RECOVER);
 	if (myHtmlDoc == NULL) {
 		spdlog::error("Error parsing the HTML content\n");
 		myLoadStatus = false;
